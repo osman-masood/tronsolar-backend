6bb2e5b3b7869b892972c89d12221ae8f1db9ec3
Osman Masood <oamasood@gmail.com>
2020-12-26 00:24:13 -0800

    fixing the friggin docker stuff

--- app/src/server.py
+++ app/src/server.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-
+import os
 import socket
 import selectors
 import types
+import logging
 
 sel = selectors.DefaultSelector()
 HOST = ''
 PORT = 9999
 
+# def get_module_logging(mod_name):
+#     """
+#     To use this, do logging = get_module_logging(__name__)
+#     """
+#     logger = logging.getlogging(mod_name)
+#     handler = logger.StreamHandler()
+#     formatter = logger.Formatter(
+#         '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     logger.setLevel(logging.DEBUG)
+#     return logger
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
+    logging.info(f"accepted connection from {addr}")
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
@@ -26,12 +42,13 @@ def service_connection(key, mask):
         if recv_data:
             data.outb += recv_data
         else:
-            print("closing connection to", data.addr)
+            logging.info(f"closing connection to {data.addr}")
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print("From ", data.addr, ": ", repr(data.outb))
+            logging.info(f"From {data.addr}: {repr(data.outb)}")
+            # print("From ", data.addr, ": ", repr(data.outb))
             # print("echoing", repr(data.outb), "to", data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
@@ -46,9 +63,11 @@ host, port = HOST, PORT
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-print("listening on", (host, port))
+logging.info(f"listening on ({host}, {port})")
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
+
+print("TESTING")
 
 try:
     while True:
@@ -60,8 +79,8 @@ try:
                 else:
                     service_connection(key, mask)
         except ConnectionResetError:
-            print("Client closed the connection")
+            logging.info("Client closed the connection")
 except KeyboardInterrupt:
-    print("caught keyboard interrupt, exiting")
+    logging.info("caught keyboard interrupt, exiting")
 finally:
     sel.close()
--- tcpclient.py
+++ tcpclient.py
@@ -5,8 +5,8 @@ host = socket.gethostname()
 port = 9999                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-print("Sending the data")
 for index in range(10):
+    print("Sending the data")
     s.sendall(b'Hello, world ')
     s.sendall(str.encode(str(index)))
     data = s.recv(1024)
