3aa1ae4d454e400dfef04a1db874e9fe824b86e1
Osman Masood <oamasood@gmail.com>
2020-12-25 21:42:51 -0800

    basic HTTP server, logs stuff

--- app.py
+++ app.py
@@ -1,46 +1,20 @@
 # app.py
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
-
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    return jsonify({
-        "ERROR": "no name found, please send a name."
-    })
+logger = logging.getLogger('tcpserver')
+logger.setLevel(logging.DEBUG)
 
 
 # A welcome message to test our server
-@app.route('/')
+@app.route('/', methods=['GET', 'POST', 'PUT', 'PATCH'])
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    logger.warning(f"Request args: {request.args.to_dict()}")
+    logger.warning(f"Request body: {request.get_data()}")
+    return jsonify({"message": "Success"})
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
